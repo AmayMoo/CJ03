3b2e68ca5a111ca1a3fb140b2f655812a3f7a498
AmayMoo <124528334+AmayMoo@users.noreply.github.com>
2023-02-17 02:36:03 +0100

    Add files via upload

---
+++ ex05/ft_strlen.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+	int count;
+
+	count = 0;
+	while(str[count])
+		count++;
+	return(count);
+}
+/*
+int main(void)
+{
+	printf("%d\n", ft_strlen("vicius"));
+}
+/*
